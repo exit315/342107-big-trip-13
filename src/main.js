c502e30055daeccd5f03d7d5eda3347ff022a284
a_karnaukhova <karn-av@protonmail.com>
2020-12-05 14:53:15 +0300

    Добавляет смену события на форму редактирования

--- src/main.js
+++ src/main.js
@@ -1,33 +1,56 @@
-import {createEditEventFormTemplate} from "./view/edit-form.js";
-import {eventPointTemplate} from "./view/event-point.js";
-import {createFilterTemplate} from "./view/filters.js";
-import {createMainMenu} from "./view/site-menu.js";
-import {createSortingTemplate} from "./view/sorting.js";
-import {createSummaryInfo} from "./view/trip-info.js";
+import EditEventPointView from "./view/edit-form.js";
+import EventPointView from "./view/event-point.js";
+import FilterView from "./view/filters.js";
+import SiteMenuView from "./view/site-menu.js";
+import SortingView from "./view/sorting.js";
+import TripInfoView from "./view/trip-info.js";
 import {generateEventPoint} from "./mock/event-point.js";
+import {render, RenderPosition} from "./utils/utils.js";
 
 
 const headerElement = document.querySelector(`.page-header`);
 const siteMenuWrapper = headerElement.querySelector(`.trip-main`);
-const siteMenuControllers = siteMenuWrapper.querySelector(`.trip-controls`);
+const siteMenuControls = siteMenuWrapper.querySelector(`.trip-controls`);
 const mainElement = document.querySelector(`.page-main`);
 const mainElementContent = mainElement.querySelector(`.trip-events`);
+const eventsListComponent = mainElement.querySelector(`.trip-events__list`);
 const EVENTS_COUNT = 20;
 
-const render = (container, element, place) => {
-  container.insertAdjacentHTML(place, element);
+render(siteMenuWrapper, new TripInfoView().getElement(), RenderPosition.AFTERBEGIN);
+render(siteMenuControls, new SiteMenuView().getElement(), RenderPosition.BEFOREEND);
+render(siteMenuControls, new FilterView().getElement(), RenderPosition.BEFOREEND);
+render(mainElementContent, new SortingView().getElement(), RenderPosition.AFTERBEGIN);
+
+const eventsList = new Array(EVENTS_COUNT).fill().map(generateEventPoint);
+
+const renderEventPoint = (eventsListElement, eventItem) => {
+  const eventComponent = new EventPointView(eventItem);
+  const eventEditComponent = new EditEventPointView(eventItem);
+
+  const replaceCardToForm = () => {
+    eventsListElement.replaceChild(eventEditComponent.getElement(), eventComponent.getElement());
+  };
+
+  const replaceFormToCard = () => {
+    eventsListElement.replaceChild(eventComponent.getElement(), eventEditComponent.getElement());
+  };
+
+  eventComponent.getElement().querySelector(`.event__rollup-btn`).addEventListener(`click`, () => {
+    replaceCardToForm();
+  });
+
+  eventEditComponent.getElement().querySelector(`.event__rollup-btn`).addEventListener(`click`, (evt) => {
+    replaceFormToCard();
+  });
+
+  eventEditComponent.getElement().addEventListener(`submit`, (evt) => {
+    evt.preventDefault();
+    replaceFormToCard();
+  });
+
+  render(eventsListElement, eventComponent.getElement(), RenderPosition.BEFOREEND);
 };
 
-const events = new Array(EVENTS_COUNT).fill().map(generateEventPoint);
-
-render(siteMenuWrapper, createSummaryInfo(), `afterbegin`);
-render(siteMenuControllers, createMainMenu(), `beforeend`);
-render(siteMenuControllers, createFilterTemplate(), `beforeend`);
-render(mainElementContent, createSortingTemplate(), `beforeend`);
-render(mainElementContent, createEditEventFormTemplate(events[0]), `beforeend`);
-
 for (let i = 1; i < EVENTS_COUNT; i++) {
-  render(mainElementContent, eventPointTemplate(events[i]), `beforeend`);
+  renderEventPoint(eventsListComponent, eventsList[i]);
 }
-
-render(mainElementContent, createEditEventFormTemplate(), `beforeend`);
