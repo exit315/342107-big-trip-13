c503c6a0042bbc5ff4b608ca745071a28b7b8029
a_karnaukhova <karn-av@protonmail.com>
2021-01-03 21:11:39 +0300

    Добавляет константы

--- src/utils/const.js
+++ src/utils/const.js
@@ -387,3 +387,15 @@ export const Mode = {
   DEFAULT: `DEFAULT`,
   EDITING: `EDITING`
 };
+
+export const UserAction = {
+  UPDATE_POINT: `UPDATE_POINT`,
+  ADD_POINT: `ADD_POINT`,
+  DELETE_POINT: `DELETE_POINT`
+};
+
+export const UpdateType = {
+  PATCH: `PATCH`,
+  MINOR: `MINOR`,
+  MAJOR: `MAJOR`
+};
