10debb980c09ba78f286653851972b28e0729e49
a_karnaukhova <karn-av@protonmail.com>
2021-01-04 12:37:58 +0300

    Добавляет методы модели в презентер, методы очистки списка точек

--- src/presenter/trip.js
+++ src/presenter/trip.js
@@ -2,8 +2,9 @@ import EventsListView from "../view/events-list.js";
 import SortingView from "../view/sorting.js";
 import NoPointsView from "../view/no-points.js";
 import PointPresenter from "./point.js";
-import {render, RenderPosition} from "../utils/render.js";
-import {updateItem} from "../utils/utils.js";
+import {render, RenderPosition, remove} from "../utils/render.js";
+import {UserAction, UpdateType} from "../utils/const.js";
+
 
 export default class Trip {
   constructor(eventsListContainer, pointsModel) {
@@ -13,8 +14,12 @@ export default class Trip {
     this._sortingComponent = new SortingView();
     this._noPointsComponent = new NoPointsView();
     this._pointPresenter = {};
-    this._handleEventChange = this._handleEventChange.bind(this);
+
+    this._handleViewAction = this._handleViewAction.bind(this);
+    this._handleModelEvent = this._handleModelEvent.bind(this);
     this._handleModeChange = this._handleModeChange.bind(this);
+
+    this._pointsModel.addObserver(this._handleModelEvent);
   }
 
   init() {
@@ -27,8 +32,34 @@ export default class Trip {
     return this._pointsModel.getPoints();
   }
 
-  _handleEventChange(updatedEvent) {
-    this._pointPresenter[updatedEvent.id].init(updatedEvent);
+  _handleViewAction(actionType, updateType, update) {
+    switch (actionType) {
+      case UserAction.UPDATE_POINT:
+        this._pointsModel.updatePoint(updateType, update);
+        break;
+      case UserAction.ADD_POINT:
+        this._pointsModel.addPoint(updateType, update);
+        break;
+      case UserAction.DELETE_POINT:
+        this._pointsModel.deletePoint(updateType, update);
+        break;
+    }
+  }
+
+  _handleModelEvent(updateType, data) {
+    switch (updateType) {
+      case UpdateType.PATCH:
+        this._pointPresenter[data.id].init(data);
+        break;
+      case UpdateType.MINOR:
+        this._clearTripBoard();
+        this._renderTripBoard();
+        break;
+      case UpdateType.MAJOR:
+        this._clearTripBoard();
+        this._renderTripBoard();
+        break;
+    }
   }
 
   _handleModeChange() {
@@ -46,13 +77,30 @@ export default class Trip {
   }
 
   _renderPoint(eventPoint) {
-    const pointPresenter = new PointPresenter(this._eventsListComponent, this._handleEventChange, this._handleModeChange);
+    const pointPresenter = new PointPresenter(this._eventsListComponent, this._handleViewAction, this._handleModeChange);
     pointPresenter.init(eventPoint);
     this._pointPresenter[eventPoint.id] = pointPresenter;
   }
 
   _renderPointsList(points) {
     points.forEach((point) => this._renderPoint(point));
+  }
+
+  _clearPointsList() {
+    Object
+      .values(this._pointPresenter)
+      .forEach((presenter) => presenter.destroy());
+    this._pointPresenter = {};
+  }
+
+  _clearTripBoard() {
+    Object
+      .values(this._pointPresenter)
+      .forEach((presenter) => presenter.destroy());
+    this._pointPresenter = {};
+
+    remove(this._sortingComponent);
+    remove(this._noPointsComponent);
   }
 
   _renderTripBoard() {
