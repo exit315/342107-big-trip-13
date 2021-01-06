d9f801adfd0ffce329ae00436c61477f24f31648
a_karnaukhova <karn-av@protonmail.com>
2021-01-06 20:16:35 +0300

    Исправляет тип обновления точки

--- src/presenter/point.js
+++ src/presenter/point.js
@@ -118,7 +118,7 @@ export default class Point {
   _submitFormHandler(eventPoint) {
     this._changeData(
         UserAction.UPDATE_POINT,
-        UpdateType.MINOR,
+        UpdateType.PATCH,
         eventPoint
     );
     this._replaceFormToCard();
