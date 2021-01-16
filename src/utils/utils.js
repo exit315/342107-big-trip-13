6f60c89b3ddbb5b69b7f7b0a1db4913f183c6dad
a_karnaukhova <karn-av@protonmail.com>
2021-01-16 21:15:19 +0300

    Добавляет вспомогательные функции

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -28,3 +28,54 @@ export const updateItem = (items, update) => {
     ...items.slice(index + 1)
   ];
 };
+
+export const makeItemsUniq = (items) => [...new Set(items)];
+
+export const countPointsByFeature = (points, feature) => {
+  return points.filter((point) => point.feature === feature).length;
+};
+
+export const calcStatistics = (points, uniqFeaturesArray) => {
+  const initial = 0;
+  const arr = [];
+
+  for (let i = 0; i < uniqFeaturesArray.length; i++) {
+    arr.push(points.reduce((newArr, point)=> {
+      if (point.pointType.typeOfPoint === uniqFeaturesArray[i]) {
+        newArr.push(point.feature);
+      }
+      return newArr;
+    }, []));
+  }
+
+  const sumArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].length === 1) {
+      sumArr.push(arr[i][0]);
+    } else {
+      sumArr.push(arr[i].reduce((sum, current) => {
+        return sum + current;
+      }, initial));
+    }
+  }
+
+  return sumArr;
+};
+
+export const calcSum = (arr) => {
+  const sumArr = [];
+  const initial = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].length === 1) {
+      sumArr.push(arr[i][0]);
+    } else {
+      sumArr.push(arr[i].reduce((sum, current) => {
+        return sum + current;
+      }, initial));
+    }
+  }
+
+  return sumArr;
+};
